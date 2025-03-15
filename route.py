c496fa2560e308928996a8819c2b59cabc866d85
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-03-15 07:18:52 +0300

    idfkwaid

--- route.py
+++ route.py
@@ -2,7 +2,7 @@ from helping_functions import clean_url, time_to_gmt, add_route_to_json
 
 
 class Route:
-    def __init__(self, route_id, user_id, url, time_frame, offset_to_gmt):
+    def __init__(self, route_id, user_id, url, time_frame, offset_to_gmt, new=True):
         self.route_id = route_id
         self.user_id = user_id
         self.url = clean_url(url)
@@ -12,16 +12,16 @@ class Route:
             self.time_frame = time_to_gmt(time_frame, offset_to_gmt)
         self.offset_to_gmt = offset_to_gmt
 
-        start, end = self.time_frame
-
-        if start < end:
-            for i in range(start, end):
-                add_route_to_json(i, self)
-        else:
-            for i in range(start, 24):  # Loop from start to midnight
-                add_route_to_json(i, self)
-            for i in range(0, end):  # Loop from midnight to end
-                add_route_to_json(i, self)
+        if new:
+            start, end = self.time_frame
+            if start < end:
+                for i in range(start, end):
+                    add_route_to_json(i, self)
+            else:
+                for i in range(start, 24):  # Loop from start to midnight
+                    add_route_to_json(i, self)
+                for i in range(0, end):  # Loop from midnight to end
+                    add_route_to_json(i, self)
 
     def to_dict(self):
         return {
@@ -39,7 +39,8 @@ class Route:
             user_id=data["user_id"],
             url=data["url"],
             time_frame=data["time_frame"],
-            offset_to_gmt=data["offset_to_gmt"]
+            offset_to_gmt=data["offset_to_gmt"],
+            new=False
         )
 
-r=Route(1,2,"https://maps.app.goo.gl/huJRcEhmcg45BB439", "1-4", 3)
+#r=Route(3,2,"https://maps.app.goo.gl/QwhAoBWBYkn9dic5A", "6-8", 3)
